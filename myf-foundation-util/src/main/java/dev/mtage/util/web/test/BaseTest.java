cfac3e0ba1d29a03272cd9ca413feec3346d0aaa
majiaxin110 <majiaxin110@gmail.com>
2020-07-19 23:35:10 +0800

    版本号同步；测试时的颜色

--- myf-foundation-util/src/main/java/dev/mtage/util/web/test/BaseTest.java
+++ myf-foundation-util/src/main/java/dev/mtage/util/web/test/BaseTest.java
@@ -3,6 +3,7 @@ package dev.mtage.util.web.test;
 import com.alibaba.fastjson.JSON;
 import dev.mtage.util.web.SimpleResult;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 
 import java.util.List;
 
@@ -12,6 +13,11 @@ import java.util.List;
  * @since 2020/2/8 12:10
  */
 public class BaseTest {
+    @BeforeAll
+    public static void ansi() {
+        System.setProperty("spring.output.ansi.enabled", "ALWAYS");
+    }
+
     protected void checkResultAndOut(SimpleResult<?> result) {
         Assertions.assertTrue(result.getSuccess());
         System.out.println(JSON.toJSONString(result, true));
