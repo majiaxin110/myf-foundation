1dc683c8e2fc34145e9e5044107bc88409bbe815
majiaxin110 <majiaxin110@gmail.com>
2020-09-03 15:56:52 +0800

    codecov

---
+++ myf-foundation-util/src/test/java/dev/mtage/util/web/SimpleResultTest.java
@@ -0,0 +1,17 @@
+package dev.mtage.util.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author mtage
+ * @since 2020/9/3 15:54
+ */
+class SimpleResultTest {
+    @Test
+    void test() {
+        SimpleResult<String> result = SimpleResult.success("result");
+        assertNotNull(result);
+    }
+}
