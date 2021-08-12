7c5f126ea5eccc4dea5d37e558a50751d03b3daa
majiaxin110 <majiaxin110@gmail.com>
2021-08-12 14:23:55 +0800

    enhance AssertUtil

--- myf-foundation-util/src/main/java/dev/mtage/util/AssertUtil.java
+++ myf-foundation-util/src/main/java/dev/mtage/util/AssertUtil.java
@@ -7,10 +7,12 @@ import dev.mtage.error.CommonSysException;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * 断言工具类
  * 之所以不直接使用 guava 的 preconditions，是因为其抛出的Exception各种各样，又无法添加errorCode
+ *
  * @author mtage
  * @since 2019/9/3 21:44
  **/
@@ -23,10 +25,26 @@ public class AssertUtil {
      * @throws CommonSysException if {@code obj} is null, with code and message
      */
     public static <T> void checkNotNull(T obj, String errorCode, String errorMessage) {
+        checkNotNull(obj, errorCode, errorMessage, null);
+    }
+
+    /***
+     * Ensures that an object reference passed as a parameter to the calling method is NOT null based on Guava.
+     * @param obj an object reference
+     * @param errorCode the exception code to use if the check fails
+     * @param errorMessage the exception message to use if the check fails
+     * @param preAction the action need to execute before throwing the exception
+     * @throws CommonSysException if {@code obj} is null, with code and message
+     */
+    public static <T> void checkNotNull(T obj, String errorCode, String errorMessage, Consumer<CommonSysException> preAction) {
         try {
             Preconditions.checkNotNull(obj, errorMessage);
         } catch (NullPointerException e) {
-            throw new CommonSysException(errorCode, errorMessage, e);
+            CommonSysException sysException = new CommonSysException(errorCode, errorMessage, e);
+            if (Objects.nonNull(preAction)) {
+                preAction.accept(sysException);
+            }
+            throw sysException;
         }
     }
 
@@ -42,22 +60,41 @@ public class AssertUtil {
 
     /**
      * Ensures that an object reference passed as a parameter to the calling method is null based on Guava.
-     * @param obj an object reference
-     * @param errorCode the exception code to use if the check fails
+     *
+     * @param obj          an object reference
+     * @param errorCode    the exception code to use if the check fails
      * @param errorMessage the exception message to use if the check fails
+     * @param preAction    the action need to execute before throwing the exception
      * @throws CommonSysException if {@code obj} is not null, with code and message
      */
-    public static <T> void checkNull(T obj, String errorCode, String errorMessage) {
+    public static <T> void checkNull(T obj, String errorCode, String errorMessage, Consumer<CommonSysException> preAction) {
         try {
             Preconditions.checkArgument(Objects.isNull(obj), errorMessage);
         } catch (IllegalArgumentException e) {
-            throw new CommonSysException(errorCode, errorMessage, e);
+            CommonSysException sysException = new CommonSysException(errorCode, errorMessage, e);
+            if (Objects.nonNull(preAction)) {
+                preAction.accept(sysException);
+            }
+            throw sysException;
         }
     }
 
     /**
      * Ensures that an object reference passed as a parameter to the calling method is null based on Guava.
-     * @param obj an object reference
+     *
+     * @param obj          an object reference
+     * @param errorCode    the exception code to use if the check fails
+     * @param errorMessage the exception message to use if the check fails
+     * @throws CommonSysException if {@code obj} is not null, with code and message
+     */
+    public static <T> void checkNull(T obj, String errorCode, String errorMessage) {
+        checkNull(obj, errorCode, errorMessage, null);
+    }
+
+    /**
+     * Ensures that an object reference passed as a parameter to the calling method is null based on Guava.
+     *
+     * @param obj          an object reference
      * @param errorMessage the exception message to use if the check fails
      * @throws CommonSysException if {@code obj} is not null, with message
      */
@@ -65,22 +102,40 @@ public class AssertUtil {
         checkNull(obj, null, errorMessage);
     }
 
+    /**
+     * Ensures that a string is not blank based Apache Commons.
+     *
+     * @param str
+     * @param errorCode
+     * @param errorMessage
+     * @param preAction the action need to execute before throwing the exception
+     * @throws CommonSysException if {@code obj} is blank, with code and message
+     */
+    public static void checkNotBlank(String str, String errorCode, String errorMessage, Consumer<CommonSysException> preAction) {
+        if (StringUtils.isBlank(str)) {
+            CommonSysException sysException = new CommonSysException(errorCode, errorMessage);
+            if (Objects.nonNull(preAction)) {
+                preAction.accept(sysException);
+            }
+            throw sysException;
+        }
+    }
 
     /**
      * Ensures that a string is not blank based Apache Commons.
+     *
      * @param str
      * @param errorCode
      * @param errorMessage
      * @throws CommonSysException if {@code obj} is blank, with code and message
      */
     public static void checkNotBlank(String str, String errorCode, String errorMessage) {
-        if (StringUtils.isBlank(str)) {
-            throw new CommonSysException(errorCode, errorMessage);
-        }
+        checkNotBlank(str, errorCode, errorMessage, null);
     }
 
     /**
      * Ensures that a string is not blank based Apache Commons.
+     *
      * @param str
      * @param errorMessage
      * @throws CommonSysException if {@code obj} is blank, with message
@@ -91,19 +146,39 @@ public class AssertUtil {
 
     /**
      * Ensures that two object reference passed are equals
+     *
+     * @param objA
+     * @param objB
+     * @param errorCode
+     * @param errorMessage
+     * @param preAction the action need to execute before throwing the exception
+     */
+    public static void checkEquals(Object objA, Object objB, String errorCode, String errorMessage,
+                                   Consumer<CommonSysException> preAction) {
+        if (!Objects.equals(objA, objB)) {
+            CommonSysException sysException = new CommonSysException(errorCode, errorMessage);
+            if (Objects.nonNull(preAction)) {
+                preAction.accept(sysException);
+            }
+            throw sysException;
+        }
+    }
+
+    /**
+     * Ensures that two object reference passed are equals
+     *
      * @param objA
      * @param objB
      * @param errorCode
      * @param errorMessage
      */
     public static void checkEquals(Object objA, Object objB, String errorCode, String errorMessage) {
-        if (!Objects.equals(objA, objB)) {
-            throw new CommonSysException(errorCode, errorMessage);
-        }
+        checkEquals(objA, objB, errorCode, errorMessage, null);
     }
 
     /**
      * Ensures that two object reference passed are equals
+     *
      * @param objA
      * @param objB
      * @param errorMessage
@@ -114,41 +189,77 @@ public class AssertUtil {
 
     /**
      * Ensures that an object is instance of a class
+     *
      * @param obj
      * @param type
      */
-    public static void checkClass(Object obj, Class<?> type, String errorCode, String errorMessage) {
+    public static void checkClass(Object obj, Class<?> type, String errorCode, String errorMessage,
+                                  Consumer<CommonSysException> preAction) {
         if (!obj.getClass().equals(type)) {
-            throw new CommonSysException(errorCode, errorMessage);
+            CommonSysException sysException = new CommonSysException(errorCode, errorMessage);
+            if (Objects.nonNull(preAction)) {
+                preAction.accept(sysException);
+            }
+            throw sysException;
         }
     }
 
     /**
      * Ensures that an object is instance of a class
+     *
+     * @param obj
+     * @param type
+     */
+    public static void checkClass(Object obj, Class<?> type, String errorCode, String errorMessage) {
+        checkClass(obj, type, errorCode, errorMessage, null);
+    }
+
+    /**
+     * Ensures that an object is instance of a class
+     *
      * @param obj
      * @param type
      */
     public static void checkClass(Object obj, Class<?> type, String errorCode) {
-        checkClass(obj, type,null, errorCode);
+        checkClass(obj, type, null, errorCode);
     }
 
     /**
      * Ensures that {@code expression} is {@code true}
+     *
+     * @param expression
+     * @param errorCode
+     * @param errorMessage
+     * @param preAction the action need to execute before throwing the exception
+     * @throws CommonSysException if {@code expression} is not {@code true}, with code and message
+     */
+    public static void verify(boolean expression, String errorCode, String errorMessage, Consumer<CommonSysException> preAction) {
+        try {
+            Verify.verify(expression, errorMessage);
+        } catch (VerifyException e) {
+            CommonSysException sysException = new CommonSysException(errorCode, errorMessage, e);
+            if (Objects.nonNull(preAction)) {
+                preAction.accept(sysException);
+            }
+            throw sysException;
+        }
+    }
+
+    /**
+     * Ensures that {@code expression} is {@code true}
+     *
      * @param expression
      * @param errorCode
      * @param errorMessage
      * @throws CommonSysException if {@code expression} is not {@code true}, with code and message
      */
     public static void verify(boolean expression, String errorCode, String errorMessage) {
-        try {
-            Verify.verify(expression, errorMessage);
-        } catch (VerifyException e) {
-            throw new CommonSysException(errorCode, errorMessage, e);
-        }
+        verify(expression, errorCode, errorMessage, null);
     }
 
     /**
      * Ensures that {@code expression} is {@code true}
+     *
      * @param expression
      * @param errorMessage
      * @throws CommonSysException if {@code expression} is not {@code true}, with message
